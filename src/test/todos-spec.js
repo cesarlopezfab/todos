2291d0275f8693ce7e78ed6a78eade14258b43c4
cesarlopezfa <cesarlopezfa@gmail.com>
2016-04-11 23:47:03 +0200

    Add es2015 support

--- src/test/todos-spec.js
+++ src/test/todos-spec.js
@@ -1,62 +1,67 @@
-var chai = require('chai');
+(() => {
+  'use strict';
 
-function Todo() {
-  return {
-    checked: false,
-    toggle: function() {
-      this.checked = !this.checked;
-    }
-  };
-}
+  const chai = require('chai');
 
-function Todos() {
-  var todos = [];
-  return {
-    add: function (todo) {
-      todos.push(todo);
-    },
-    get: function (index) {
-      return todos[index];
-    }
-  };
-}
-
-describe('Todos', function () {
-
-  it('adds one todo to todos', function () {
-    var todos = new Todos();
-    var todo = {
-      description: 'My first todo'
+  function Todo() {
+    return {
+      checked: false,
+      toggle: function () {
+        this.checked = !this.checked;
+      }
     };
-    todos.add(todo);
-    chai.expect(todos.get(0)).to.deep.equal(todo);
+  }
+
+  function Todos() {
+    let todos = [];
+    return {
+      add: function (todo) {
+        todos.push(todo);
+      },
+      get: function (index) {
+        return todos[index];
+      }
+    };
+  }
+
+  describe('Todos', () => {
+
+    it('adds one todo to todos', () => {
+      const todos = new Todos();
+      const todo = {
+        description: 'My first todo'
+      };
+      todos.add(todo);
+      chai.expect(todos.get(0)).to.deep.equal(todo);
+    });
+
+    it('adds two todos to todos', () => {
+      const todos = new Todos();
+      const todo1 = {
+        description: 'My first todo'
+      };
+      const todo2 = {
+        description: 'My second todo'
+      };
+      todos.add(todo1);
+      todos.add(todo2);
+      chai.expect(todos.get(0)).to.deep.equal(todo1);
+      chai.expect(todos.get(1)).to.deep.equal(todo2);
+    });
+
   });
 
-  it('adds two todos to todos', function () {
-    var todos = new Todos();
-    var todo1 = {
-      description: 'My first todo'
-    };
-    var todo2 = {
-      description: 'My second todo'
-    };
-    todos.add(todo1);
-    todos.add(todo2);
-    chai.expect(todos.get(0)).to.deep.equal(todo1);
-    chai.expect(todos.get(1)).to.deep.equal(todo2);
+  describe('Todo', () => {
+    it('toggles', () => {
+      const todo = new Todo();
+      chai.expect(todo.checked).to.be.false;
+      todo.toggle();
+      chai.expect(todo.checked).to.be.true;
+      todo.toggle();
+      chai.expect(todo.checked).to.be.false;
+    });
+
   });
 
-});
-
-describe('Todo', function() {
-  it('toggles', function () {
-    var todo = new Todo();
-    chai.expect(todo.checked).to.be.false;
-    todo.toggle();
-    chai.expect(todo.checked).to.be.true;
-    todo.toggle();
-    chai.expect(todo.checked).to.be.false;
-  });
-
-});
+})();
 
